cad2f391850debdf48390bb4b34fd178eccfcc50
ArmandoJosueMeza <arjome196@gmail.com>
2018-11-29 01:20:00 -0600

    Implementacion de Firebase

--- app/src/main/java/apps/kata/spideymessenger/LoginActivity.kt
+++ app/src/main/java/apps/kata/spideymessenger/LoginActivity.kt
@@ -3,20 +3,31 @@ package apps.kata.spideymessenger
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity: AppCompatActivity() {
-    //Creando pantalla de Login
+class LoginActivity:AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_login)
 
-        lblVolverRegistro_Login.setOnClickListener {
-            // Mostrando Pantalla de Registro
+        //Accion al hacer click en el boton Iniciar Sesión
+        btnIniciar_login.setOnClickListener {
+            val correo = txtCorreo_login.text.toString()
+            val password = txtPassword_login.text.toString()
+
+            // Iniciando Login de usuario ya registrado
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(correo, password)
+
+        }
+
+
+
+        // Volver a pantalla de registro
+        lblVolverRegistro_login.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
-
 }
--- app/src/main/java/apps/kata/spideymessenger/MainActivity.kt
+++ app/src/main/java/apps/kata/spideymessenger/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,21 +14,58 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Accion al hacer click en el boton registro
-        btnRegistro_login.setOnClickListener {
-            // Creacion de las variables para el inicio de registro
-            val nombreUsuario = txtNombreUsuario_registro.text.toString()
-            val correo = txtCorreo_registro.text.toString()
-            val password = txtPassword_registro.text.toString()
-            //Toast.makeText(this, "Registrado con exito", Toast.LENGTH_SHORT).show()
-            // Implementacion de Firebase
+        btnRegistro_registro.setOnClickListener {
+            crearRegistro()
 
         }
-
-        // Accion al hacer click en la etiqueta "Ya tengo una cuenta"
+        // Mostrando pantalla de Login
         lblTieneCuenta_registro.setOnClickListener {
-            // Mostrando pantalla de Login
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun crearRegistro(){
+        // Creacion de las variables para el inicio de registro
+        val nombreUsuario = txtUsuario_registro.text.toString()
+        val correo = txtCorreo_registro.text.toString()
+        val password = txtPassword_registro.text.toString()
+
+        // Validar que los valores de las variables nombreUsuario, correo y password no
+        // se introduzcan con valores vacios o incorrectos
+        if (nombreUsuario.isEmpty()){
+            Toast.makeText(this, "Debe de ingresar información en : Nombre de usuario", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (correo.isEmpty()){
+            Toast.makeText(this, "Debe de ingresar información en : Correo", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (password.isEmpty()){
+            Toast.makeText(this, "Debe de ingresar información en : Contraseña", Toast.LENGTH_LONG).show()
+            return
+        }
+
+
+        // Implementacion de Firebase
+        // Creando una instancia de auteticacion con el correo y la contraseña
+        // Obteniendo los valores de las variables correo y password
+        // Creando registro de usuario
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(correo, password)
+            // Si el proceso de autenticacion no es completado con exito devolver al usurio para que lo complete
+            .addOnCompleteListener {
+                if (!it.isSuccessful){
+                    return@addOnCompleteListener
+                } else {
+                    // si es completado con exito
+                    Toast.makeText(this, "Se ha creado exitosamente el registro con un id de Usuario: ${it.result!!.user.uid}", Toast.LENGTH_LONG).show()
+                }
+            }
+            // Manipulando la Excepcion de ".addOnFailureListener", si falla al crear un usuario
+            .addOnFailureListener{
+                Toast.makeText(this, "Fallo al crear usuario: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
